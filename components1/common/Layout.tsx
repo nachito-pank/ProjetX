377f1a509e5c1d68f4b8f37c804d48d70f51662d
Okemba Smiley Chado <okembasmileychado@gmail.com>
2026-03-10 20:43:56 +0100

    Refactor Navbar import in Layout component for consistency

--- components1/common/Layout.tsx
+++ components1/common/Layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Navbar } from './Navbar';
+import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import { ToastProvider } from './Toast';
 
